2f9e9652a282fad6ed94d1406ec1349581799ae1
girinathtime <52603598+girinathtime@users.noreply.github.com>
2020-05-05 20:25:46 +0530

    First Python program, Comments and indentation.
    This is the file which i used in Video no:01 to explain about first python program, comments and indentation in python.

---
+++ firstprogram.py
@@ -0,0 +1,25 @@
+#Video no:01
+'''To run python file in terminal or CMD
+python3 filename.py is used''' 
+print("Hello world")
+#Other way to run python file is to use an IDE ex:VSCode
+#print() is an in-built function to display infomation to the end user
+print("Hello Python")  
+
+'''This  is an example of 
+ multi line comment 
+ using single quotes
+'''
+"""This is an example 
+of  multi line comment
+using double quotes
+"""
+
+#Indentation in python
+""" Indentation is used to specify a block of code
+in python. Indentation means 4 spaces
+"""
+x = 10
+if x>10:
+    print("hello")
+print("hiii")
