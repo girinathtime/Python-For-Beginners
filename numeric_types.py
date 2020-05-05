b8f568b7f1d5f6151f07f0d586cbe660236862a5
girinathtime <52603598+girinathtime@users.noreply.github.com>
2020-05-05 20:32:05 +0530

    Numeric DataTypes in Python.
    In this file, you can find codes related to numeric datatypes in python

---
+++ numeric_types.py
@@ -0,0 +1,17 @@
+#Video no:02
+#Numeric datatypes in python
+
+#This is an integer type
+x = 10        # x is a varible
+print(type(x))   # type() is a function used to know the type of an object
+
+#This is an float type
+y = 3.42
+print(type(y))
+
+#This is an complex type
+z = 2+3j      #2 is real part and 3j is imaginary part
+print(type(z))
+
+print(x,y,z)  #printing the values
+
